3dfdb390f17e4137b50a5795867c354d8ef1c08e
dbaker1 <douglas.baker@burberry.com>
2018-04-13 17:04:52 +0100

    first commit

---
+++ code/D0ugla8_Bot/nodebot/config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: '1KsElPs7IPasbJ1kLzxra8JnF',
+  consumer_secret: 'kdKViVxsAARYix0FZD9nD0snw4cQzIdKyuBR5z1snaHrOBQXlC',
+  access_token: '838043886073311234-e9wbGGVDrDEJgYTmJcoyFmVnmm4nC49',
+  access_token_secret: 'NrHMOxtxKuKQE34v6afwNeA4q6ZoXyy83IMC2Sa5MImfp'
+}
---
+++ code/D0ugla8_Bot/nodebot/index.js
@@ -0,0 +1,1 @@
+<script src="https://gist.github.com/debashisbarman/bffe0f6cd3c0fd2fe40e.js"></script>
